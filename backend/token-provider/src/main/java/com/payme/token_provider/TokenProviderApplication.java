fb845a5a4d34954bb4e19ad989c99ea459145eee
Yohannes <yohannesm8814@gmail.com>
2025-04-22 18:11:43 -0600

    figured out how to integrate the nexus repository, and pull the library from there to be used in each service

--- backend/token-provider/src/main/java/com/payme/token_provider/TokenProviderApplication.java
+++ backend/token-provider/src/main/java/com/payme/token_provider/TokenProviderApplication.java
@@ -2,9 +2,8 @@ package com.payme.token_provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
-@Profile("dev")
+
 @EnableScheduling
 @SpringBootApplication
 public class TokenProviderApplication {
